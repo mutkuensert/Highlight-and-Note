58869b17c326ce785ed5debb42082a4b2128ebf4
M. Utku Ensert <mutkuensert@hotmail.com>
2024-12-26 00:00:03 +0300

    build: upgrade project version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.mutkuensert.highlightandnote"
         minSdk = 23
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
