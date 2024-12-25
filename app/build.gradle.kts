34626bbe0f8bd1bb194c4d5cb703b6e03f9bc224
M. Utku Ensert <mutkuensert@hotmail.com>
2024-12-25 21:53:51 +0300

    build: update dependency versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.mutkuensert.highlightandnote"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mutkuensert.highlightandnote"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 5
         versionName = "1.0.4"
 
