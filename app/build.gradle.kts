f1c1e06312e887586d1bbf86fd6b91f787a03e53
M. Utku Ensert <mutkuensert@hotmail.com>
2024-12-26 13:47:55 +0300

    build: update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.mutkuensert.highlightandnote"
         minSdk = 23
         targetSdk = 35
-        versionCode = 6
+        versionCode = 7
         versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
